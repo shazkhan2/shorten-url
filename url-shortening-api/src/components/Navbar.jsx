51d752e311d7aa1118a57a098cdfb183daa7d020
shazkhan2 <seewardak@hotmail.com>
2024-04-12 23:23:10 +0200

    Navbar reworked

--- url-shortening-api/src/components/Navbar.jsx
+++ url-shortening-api/src/components/Navbar.jsx
@@ -1,32 +1,29 @@
-import React, { useState } from 'react';
-import logo from "../images/logo.svg";
-
+import React from 'react'
+import logo from "../images/logo.svg"
 export default function Navbar() {
-  // State to track whether the menu is visible or not
-  const [menuVisible, setMenuVisible] = useState(false);
-
-  // Function to toggle the visibility of the menu
-  const toggleMenu = () => {
-    setMenuVisible(!menuVisible);
-  };
-
   return (
     <>
-      <header className='header'>
-        <div>
-          <img src={logo} alt="Logo" />
-          <nav style={{ display: menuVisible ? 'block' : 'none' }}>
-            <ul>
-              <li>Features</li>
-              <li>Prices</li>
-              <li>Resources</li>
-              <li>Login</li>
-              <li>Sign Up</li>
-            </ul>
-          </nav>
-        </div>
-        <button onClick={toggleMenu}>Menu</button>
-      </header>
-    </>
-  );
+<header className='header'>
+
+    <div>
+
+        <img src = {logo} alt = "Logo" />
+<nav>
+    <ul>
+        <li>Features  </li>
+        <li>Prices  </li>
+        <li>Resources  </li>
+    </ul>
+
+</nav>
+
+    </div>
+    <ul>
+        <li>Login</li>
+        <li>Sign Up</li>
+    </ul>
+
+</header>
+  </>
+  )
 }
