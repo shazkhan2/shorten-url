1750d81f32f4fe93a9ae4f0adb5a16da40e5d083
shazkhan2 <seewardak@hotmail.com>
2024-04-12 23:05:02 +0200

    Illustration component created

--- url-shortening-api/src/App.js
+++ url-shortening-api/src/App.js
@@ -2,11 +2,13 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Boost from "./components/Boost";
 import Statistics from "./components/Statistics";
+import Illustration from "./components/Illustration";
 
 export default function App() {
   return (
      <>
      <Navbar />
+     <Illustration />
      <Statistics />
      <Boost />
      <Footer />
---
+++ url-shortening-api/src/components/Illustration.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import illustrate from "../images/illustration-working.svg";
+
+export default function Illustration() {
+  return (
+<>
+<section>
+  <div>
+    <article>
+      <h1>
+      More than just shorter links
+
+      </h1>
+      <p>
+        
+  Build your brand’s recognition and get detailed insights 
+  on how your links are performing.
+
+      </p>
+      <button>Get Started</button>
+    </article>
+<article>
+  <img src={illustrate} alt='workstation' />
+</article>
+
+  </div>
+</section>
+
+</> 
+
+)
+}
